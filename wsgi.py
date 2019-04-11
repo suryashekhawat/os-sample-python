98a6407c32929f096d1bd690c6cb31027952c2b0
Surya Pratap Singh Shekhawat <suryashekhawat@users.noreply.github.com>
2019-04-11 17:17:53 +0530

    route update

--- wsgi.py
+++ wsgi.py
@@ -2,15 +2,18 @@ from flask import Flask
 from flask import request
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "test openshift flask deployemnt"
+@application.route('/', defaults={'path': ''})
+def root(path):
+    items = []
+    mimetype = 'application/json'
+    return Response(json.dumps(items, indent=2), mimetype=mimetype)
 
-@application.route("/hello")
-def sayhello():
-    curr_route_name = request.url_rule
-    return "route {}".format(curr_route_name)
-
+@application.route('/<path:path>')
+def catch_all(path):
+    count = 1
+    mimetype = 'application/json'
+    response = {'path': path, 'hits': '%s' % count}
+    return Response(json.dumps(response, indent=2), mimetype=mimetype)
 
 
 if __name__ == "__main__":
