235b47acd28de81b6ca7a5e6156e71aeff89d2c0
Surya Pratap Singh Shekhawat <suryashekhawat@users.noreply.github.com>
2019-04-11 17:09:21 +0530

    req route name

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask import request
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "test openshift flask deployemnt"
+
+@application.route("/hello")
+def sayhello():
+    curr_route_name = request.url_rule
+    return "route {}".format(curr_route_name)
+
+
 
 if __name__ == "__main__":
     application.run()
